94f0352f552bb9e79b848072007cb442b4949847
Nicholas Wright <dozedoffagain@gmail.com>
2017-05-11 20:19:38 +0200

    Install dependencies with setuptools

--- setup.py
+++ setup.py
@@ -8,5 +8,9 @@ setup(name='d3-item-scraper',
  author='Nicholas Wright',
  author_email='dozedoffagain@gmail.com',
  packages=find_packages(),
- test_suite="test"
+ test_suite="test",
+ install_requires = [
+	'beautifulsoup4',
+	'betamax'
+ ]
 )
