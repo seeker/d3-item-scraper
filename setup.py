251e21cd87f5baa7487424b15916f6def37fa45b
Nicholas Wright <dozedoffagain@gmail.com>
2017-05-18 23:00:26 +0200

    Do not install test dependencies by default

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@
 
 from setuptools import setup, find_packages
 
+requirements = [
+    'beautifulsoup4'
+]
+
+test_requirements = [
+    'betamax'
+]
+
 setup(name='d3-item-scraper',
  version='0.1',
  description='Diablo 3 item scraper',
@@ -9,8 +17,9 @@ setup(name='d3-item-scraper',
  author_email='dozedoffagain@gmail.com',
  packages=find_packages(),
  test_suite="test",
- install_requires = [
-	'beautifulsoup4',
-	'betamax'
- ]
+ install_requires = requirements,
+ tests_require=test_requirements,
+ extras_require={
+	'dev': test_requirements
+}
 )
