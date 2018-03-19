b6a431d3a95dbe3a505075893933f388dc2daf48
Nicholas Wright <dozedoffagain@gmail.com>
2018-03-19 23:08:05 +0100

    Fixed recorded page not used for tests

--- test/testLoader.py
+++ test/testLoader.py
@@ -27,7 +27,9 @@ class TestLoader(unittest.TestCase):
         recorder = Betamax(
             session, cassette_library_dir=CASSETTE_LIBRARY_DIR
         )
-    
+
+        self.recorder = recorder
+
         with recorder.use_cassette('ring-page'):
             self.ring_page = session.get(self.url_ring).text
             
@@ -37,11 +39,13 @@ class TestLoader(unittest.TestCase):
         self.cut = Loader(BASE_URL, session)
 
     def test_load_rings(self):
-        html = self.cut.load_items_html("/en/item/ring/")
+        with self.recorder.use_cassette('ring-page'):
+            html = self.cut.load_items_html("/en/item/ring/")
         
         self.assertIn("Halo of Karini", html)
         
     def test_load_item_index(self):
-        html = self.cut.load_item_index()
+        with self.recorder.use_cassette('item-page'):
+            html = self.cut.load_item_index()
         
         self.assertIn("Rings", html)
