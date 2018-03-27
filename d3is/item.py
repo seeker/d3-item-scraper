ead2b5045a37303c6985ffb547bb3d17cabbdd58
Nicholas Wright <dozedoffagain@gmail.com>
2018-03-27 09:37:33 +0200

    Items can now store class restrictions

--- d3is/item.py
+++ d3is/item.py
@@ -3,12 +3,23 @@ class Item(object):
     Class that holds item data.
     """
 
-    def __init__(self, name, affix):
+    def __init__(self, name, affix, class_restriction = None):
+        """
+        Create a new item instance to store data.
+        :param name: the name of the item
+        :param affix: the legendary affix of a item
+        :param class_restriction: the character class this item is restricted to. None means the affix is not restricted
+         to a class. Other restrictions (e.g. weapon type) still apply
+        """
         self.name = name.strip()
         self.affix = affix.strip()
+        if class_restriction is not None:
+            self.class_restriction = class_restriction.strip()
+        else:
+            self.class_restriction = None
     
     def __eq__(self, other):
-        return self.__dict__ == other.__dict__
+        return self.name == other.name and self.affix == other.affix
     
     def __ne__(self, other):
         return not self == other
--- test/testItem.py
+++ test/testItem.py
@@ -1,13 +1,8 @@
-'''
-Created on 11 May 2017
-
-@author: Nicholas Wright
-'''
 import unittest
 from d3is.item import Item
 
-class Test(unittest.TestCase):
 
+class Test(unittest.TestCase):
 
     def setUp(self):
         self.item_a = Item("foo", "bar")
@@ -15,12 +10,7 @@ class Test(unittest.TestCase):
         self.item_c = Item("fo", "bar")
         self.item_d = Item("foo", "baz")
         self.item_e = Item(" foo ", " baz ")
-
-
-
-    def tearDown(self):
-        pass
-
+        self.item_f = Item("foo", "bar", "baz")
 
     def test_equal(self):
         self.assertTrue(self.item_a == self.item_b)
@@ -39,6 +29,9 @@ class Test(unittest.TestCase):
 
     def test_text_trim(self):
         self.assertEqual(self.item_d, self.item_e)
+
+    def test_equal_with_different_class_restriction(self):
+        self.assertEqual(self.item_a, self.item_f)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
